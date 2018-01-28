36aaf25661cbc15e46c7780b84d80d1e32ddad35
shugui <zhenchuanding@sina.com>
2018-01-28 22:48:36 +0800

    urllib_test

--- regular.py
+++ regular.py
@@ -3,22 +3,22 @@
 import re
 
 
-s=r'ABC\-001'
-m=re.match(s,'ABC-001')
+s = r'ABC\-001'
+m = re.match(s, 'ABC-001')
 print(m)
 
-m=re.match(r'^\d{3}\-\d{3,8}$','010-12345')
+m = re.match(r'^\d{3}\-\d{3,8}$', '010-12345')
 print(m)
-s=re.split(r'[\s\,\;]+','a,b  c,,d;f')
+s = re.split(r'[\s\,\;]+', 'a,b  c,,d;f')
 print(s)
 
-m=re.match(r'^(\d{3})\-(\d{3,8})$',"010-123578")
+m = re.match(r'^(\d{3})\-(\d{3,8})$', "010-123578")
 print(m.groups())
 
-sts_re_email=r'^\w+\@\w+\.[com|cn]'
-m=re.match(sts_re_email,'someone@gmail.com')
+sts_re_email = r'^\w+\@\w+\.[com|cn]'
+m = re.match(sts_re_email, 'someone@gmail.com')
 print(m)
-m=re.match(sts_re_email,'some.one#gmail.com')
+m = re.match(sts_re_email, 'some.one#gmail.com')
 print(m)
-m=re.match(sts_re_email,'0someone@gmail.com')
+m = re.match(sts_re_email, '0someone@gmail.com')
 print(m)
