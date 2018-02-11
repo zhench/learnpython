7dd6a9700ade8c2f8abe6c73b1e9021448e668e4
Dingzhenchuan <zhenchuanding@sina.com>
2018-02-11 11:29:57 +0800

    one thread process two coroutine

--- asyncio_test.py
+++ asyncio_test.py
@@ -1,17 +1,19 @@
 # -*-codig:utf-8-*-
 import asyncio,time
+import threading
 
 @asyncio.coroutine
 def hello():
-    print("Hello world!")
+    print("Hello world! (%s)" % threading.currentThread())
     #异步调用asyncio.sleep()
-    #r=yield from asyncio.sleep(10)
-    time.sleep(2)#感觉此处和使用yield form的效果一样都是暂停了几秒
-    print('Hello again!')
+    yield from asyncio.sleep(2)
+    #time.sleep(2)#感觉此处和使用yield form的效果一样都是暂停了几秒
+    print('Hello again! (%s)' % threading.currentThread())
 
 #获取EventLoop
 loop=asyncio.get_event_loop()
+tasks=[hello(),hello()]
 #执行coroutine
-loop.run_until_complete(hello())
+loop.run_until_complete(asyncio.wait(tasks))
 #loop.run_until_complete(hello())
 loop.close()
